27a57bfd4e358e959234e7caf0a6b910894109c2
Data1010 <naj273@nyu.edu>
2015-12-06 12:07:44 -0500

    templates

--- counters_app/views.py
+++ counters_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
-
+from django.template import RequestContext, loader
 from django.http import HttpResponse
-
+from .models import Counter
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	
+	latest_counter = Counter.objects.order_by("pub_date")[:5]
+	template = loader.get_template('counters_app/Counter.html')
+	context = RequestContext(request,{'latest_counter':latest_counter,} )
+	return HttpResponse(template.render(context))
 
-# Create your views here.
