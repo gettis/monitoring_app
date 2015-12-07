3412bcabaa29a31e019c100aa81b9d8f67043e84
Aarti <aartibagul95@gmail.com>
2015-12-06 23:34:08 -0500

    Minor change

--- counters_app/models.py
+++ counters_app/models.py
@@ -6,8 +6,12 @@ from django.db import models
 class Dictionary(models.Model):
     dict_name = models.CharField(max_length = 200)
     
+    def get(self,counter_name):
+
+        entry = Counter.objects.get(counter_name=counter_name)
+    
     def __str__(self):
-        return dict_name
+        return self.dict_name
 
     
 
@@ -17,7 +21,7 @@ class Counter(models.Model):
     dict_entry = models.ForeignKey(Dictionary)
 
     counter_name = models.CharField(max_length=200)
-    counter_value = models.IntegerField()
+    counter_value = models.BigIntegerField()
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
